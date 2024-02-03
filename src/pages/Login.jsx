2e5785668d7f753e727374082cc64a250780ecd2
kaleb <kalebabiy2012@gmail.com>
2024-02-03 19:32:53 +0300

    error message updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -73,7 +73,7 @@ function Login() {
                   width: '100%',
               }}
     >
-        {message && <Alert message={message + " " + "maybe you account is not active please check your email and verify"} type="error" showIcon closable/>}
+        {message && <Alert message={`${message} verify your email to activate`} type="error" showIcon closable/>}
     </Space>
       <Card
           hoverable
